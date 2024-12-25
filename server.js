4f40ecdf5e3653a1d881b52e05fce9ad424a3384
Swarit Jain <swaritjain693@gmail.com>
2024-12-25 15:35:21 +0530

    updated by Swarit

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-const dbConfig = require("./config/dbConfig");
+const path = require('path');
+const dbConfig = require(path.join(__dirname, './config/dbConfig'));
+
 const port = process.env.PORT || 5000;
 app.use(express.json());
 
-const usersRoute = require("/routes/usersRoute");
+const usersRoute = require("./routes/usersRoute");
 
 app.use('/api/users', usersRoute);
 app.listen(port, () => console.log(`Node server Listening on port ${port}`));
