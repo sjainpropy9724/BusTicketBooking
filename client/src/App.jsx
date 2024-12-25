e5e8553fc92bb5a73532ccbac38c2a4fb2a1070f
Swarit Jain <swaritjain693@gmail.com>
2024-12-25 10:58:42 +0530

    updated by Swarit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button } from 'antd';
 
 const App = () => (
-  <div className="App">
+  <div className="App m-5">
     <h1>Shey Bus</h1>
     <Button type="primary">Button</Button>
   </div>
